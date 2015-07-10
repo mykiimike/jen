e1d6b20f30ac17b702930dd09bc9ae66d679ea86
Michael VERGOZ <mykiimike@users.noreply.github.com>
2015-07-10 14:35:41 +0200

    Update jen.js

--- jen.js
+++ jen.js
@@ -138,7 +138,7 @@ Jen.prototype.password = function(min, max, regex) {
 
 	if(min != max) {
 		cur = 0;
-		var nBi = Math.ceil(Math.log(max)/Math.LN2),
+		var nBi = Math.ceil(Math.log(max)/Math.log(2)),
 		nBy = Math.ceil(nBi/8), nByBi = nBy*8; 
 		while(cur == 0) {
 			var r = this.random(nBy)>>(nByBi-nBi);
